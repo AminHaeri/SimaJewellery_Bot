b726443aed8e8fc2dae2d7d8fbabdf458aa48a4c
AminHaeri <AminHaeri>
2021-07-18 00:24:26 +0430

    Add Constants file to manage external constants Create real main file and replace the old one with test name

---
+++ constants.py
@@ -0,0 +1,10 @@
+MESGHAL_UPRATE = 5000  # Rial
+MESGHAL_DOWNRATE = 5000  # Rial
+
+MESGHAL_TEXT = 'mesghal'
+
+MESGHAL_MSG_FA = 'لیست قیمت لحظه‌ای مثقال'
+MESGHAL_TIME_FA = 'زمان‌بروزرسانی'
+MESGHAL_BUY_FA = 'قیمت‌خرید'
+MESGHAL_SELL_FA = 'قیمت‌فروش'
+
--- main.py
+++ main.py
@@ -1,51 +1,84 @@
+import json
 import os
-
+import constants as consts
 import requests
 import telebot
-import yfinance as yf
+import datetime, pytz
+from telebot import types
 from dotenv import load_dotenv
+from dateutil import parser
+from persiantools.jdatetime import JalaliDateTime
 
 load_dotenv()
 
+API_URL = 'https://call16.tgju.org/ajax.json?2021071622-20210716230060-LkhfJvGIxwKp8Wa6k9Ti'
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    response = requests.get('https://api.github.com/')
+def fetch_finance_data():
+    response = requests.get(API_URL)
     json_response = response.json()
-    for key, value in json_response.items():
-        print(f'{key} : {value}')
-    bot.reply_to(message, "Howdy, how are you doing?")
+    return json_response
 
 
-@bot.message_handler(commands=['wsb'])
-def get_stocks(message):
-    response = ""
-    stocks = ["gme", "amc", "nok"]
-    stock_data = []
-    for stock in stocks:
-        data = yf.download(tickers=stock, period='2d', interval='1d')
-        data = data.reset_index()
-        response += f"----{stock}----\n"
-        stock_data.append([stock])
-        columns = ['stock']
-        for index, row in data.iterrows():
-            stock_position = len(stock_data) - 1
-            price = round(row['Close'], 2)
-            format_date = row['Date'].strftime('%m/%d')
-            response += f"{format_date}: {price}\n"
-            stock_data[stock_position].append(price)
-            columns.append(format_date)
-        print()
-
-        response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
-        for row in stock_data:
-            response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
-        response += "\nStock Data"
-        print(response)
-        bot.send_message(message.chat.id, response)
+def convert_currency_int(currency):
+    return int(currency.replace(',', ''))
 
 
-bot.polling()
+def convert_int_currency(intvalue):
+    return '{:,}'.format(intvalue)
+
+
+def convert_date_jalili(dt):
+    return JalaliDateTime.to_jalali(
+        datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second))
+
+
+def get_jalili_format(dt, rtl):
+    d = f"{dt.year}/{dt.month}/{dt.day}"
+    t = f"{dt.hour}:{dt.minute}:{dt.second}"
+
+    if rtl:
+        return f"{t} - {d}"
+
+    return f"{d} - {t}"
+
+
+def extract_mesghal(finance_object):
+    mesghal_object = finance_object['current'][consts.MESGHAL_TEXT]
+    return {
+        'p': convert_currency_int(mesghal_object['p']),
+        'uprate': convert_currency_int(mesghal_object['p']) + consts.MESGHAL_UPRATE,
+        'downrate': convert_currency_int(mesghal_object['p']) - consts.MESGHAL_DOWNRATE,
+        'time': convert_date_jalili(parser.parse(mesghal_object['ts']))
+    }
+
+
+def string_mesghal(mesghal_object):
+    currency = {
+        'p': convert_int_currency(mesghal_object['p']),
+        'uprate': convert_int_currency(mesghal_object['uprate']),
+        'downrate': convert_int_currency(mesghal_object['downrate'])
+    }
+    return f"<b>{consts.MESGHAL_MSG_FA}</b>                                        <a href="">&#8204;</a>\n" \
+           f"#{consts.MESGHAL_BUY_FA}: {currency['downrate']}\n" \
+           f"#{consts.MESGHAL_SELL_FA}: {currency['uprate']}\n\n" \
+           f"<b>{consts.MESGHAL_TIME_FA}:</b>\n" \
+           f"{get_jalili_format(mesghal_object['time'], False) : >100}\n<a href="">&#8204;</a>"
+
+
+@bot.message_handler(commands=['fetch'])
+def fetch_command(message):
+    response_string = string_mesghal(extract_mesghal(fetch_finance_data()))
+    print(response_string)
+    bot.send_message(chat_id='-1001598037268', text=response_string, parse_mode='HTML')
+
+
+@bot.message_handler(commands=['test'])
+def test_channel(message):
+    print('yes')
+    bot.send_message(chat_id='@amingoldtest', text='yessssss')
+
+
+bot.polling(none_stop=True)
