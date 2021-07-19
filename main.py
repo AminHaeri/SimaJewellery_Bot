2535764e583f129b0c7b6c8418cb95a7945b8be7
AminHaeri <AminHaeri>
2021-07-19 19:53:06 +0430

    Replace "TimeLoop" with "ApScheduler" lib that is much more advance and rich featured.

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import json
+import datetime
 import os
-import constants as consts
+import threading
+
 import requests
 import telebot
-import datetime, pytz
-from telebot import types
-from dotenv import load_dotenv
 from dateutil import parser
-from persiantools.jdatetime import JalaliDateTime
+from dotenv import load_dotenv
+from apscheduler.schedulers.background import BackgroundScheduler
 from persiantools import digits
+from persiantools.jdatetime import JalaliDateTime
 
-load_dotenv()
+import constants as consts
 
 API_URL = 'https://call16.tgju.org/ajax.json?2021071622-20210716230060-LkhfJvGIxwKp8Wa6k9Ti'
+load_dotenv()
+
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
+scheduler = BackgroundScheduler()
+
+commands = ['fetch', 'testfetch', 'getprefs', 'setmeshgaluprate', 'setmeshgaldownrate', 'setperiodictime']
+periodic_time = consts.PERIODIC_TIME
 mesghal = {
     'uprate': consts.MESGHAL_UPRATE,
     'downrate': consts.MESGHAL_DOWNRATE
 }
-emoji = '\U000026C4'
-
-
-def fetch_finance_data():
-    response = requests.get(API_URL)
-    json_response = response.json()
-    return json_response
 
 
 def convert_currency_int(currency):
@@ -59,6 +58,16 @@ def convert_digit_en_fa(digit):
     return digits.en_to_fa(str(digit))
 
 
+def fetch_finance_data():
+    response = requests.get(API_URL)
+    json_response = response.json()
+    return json_response
+
+
+def fetch_string():
+    return string_mesghal(extract_mesghal(fetch_finance_data()))
+
+
 def extract_mesghal(finance_object):
     mesghal_object = finance_object['current'][consts.MESGHAL_TEXT]
     return {
@@ -84,10 +93,62 @@ def string_mesghal(mesghal_object):
            f"{consts.EMPTY_TRICK}"
 
 
-def fetch_string():
-    return string_mesghal(extract_mesghal(fetch_finance_data()))
+def validate_rate(message, func):
+    rate = message.text
+    if not rate.isdigit():
+        msg = bot.reply_to(message, f"Mesghal rate should a be number. Enter that again:")
+        bot.register_next_step_handler(msg, func)
+        return False
+
+    rate = int(rate)
+    if rate < 0:
+        msg = bot.reply_to(message, f"Mesghal rate should a be a positive number. Enter that again:")
+        bot.register_next_step_handler(msg, func)
+        return False
+
+    return True
 
 
+def validate_periodic_time(message, func):
+    time = message.text
+    if not time.isdigit():
+        msg = bot.reply_to(message, f"Periodic time should a be number. Enter that again:")
+        bot.register_next_step_handler(msg, func)
+        return False
+
+    time = int(time)
+    if time < consts.PERIODIC_TIME_MIN or time > consts.PERIODIC_TIME_MAX:
+        msg = bot.reply_to(message, f"Periodic time should a be a number between "
+                                    f"{consts.PERIODIC_TIME_MIN} and {consts.PERIODIC_TIME_MAX}. Enter that again:")
+        bot.register_next_step_handler(msg, func)
+        return False
+
+    return True
+
+
+def set_mesghal_uprate(message):
+    if validate_rate(message, set_mesghal_uprate):
+        mesghal['uprate'] = int(message.text)
+        msg = f"Mesghal up rate successfully changed to <b>{convert_digit_en_fa(mesghal['uprate'])}</b>"
+        bot.reply_to(message, msg, parse_mode='HTML')
+
+
+def set_mesghal_downrate(message):
+    if validate_rate(message, set_mesghal_downrate):
+        mesghal['downrate'] = int(message.text)
+        msg = f"Mesghal down rate successfully changed to <b>{convert_digit_en_fa(mesghal['downrate'])}</b>"
+        bot.reply_to(message, msg, parse_mode='HTML')
+
+
+def set_periodic_time(message):
+    if validate_periodic_time(message, set_periodic_time):
+        global periodic_time
+        periodic_time = int(message.text)
+        msg = f"Periodic time successfully change to <b>{convert_digit_en_fa(periodic_time)}</b>"
+        bot.reply_to(message, msg, parse_mode='HTML')
+
+
+####################################### Telegram Bot Message Handlers #######################################
 @bot.message_handler(commands=['fetch'])
 def fetch_command(message):
     response_string = fetch_string()
@@ -95,42 +156,68 @@ def fetch_command(message):
     bot.send_message(chat_id=consts.CHANNEL_ID, text=response_string, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['testFetch'])
-def fetch_command(message):
+@bot.message_handler(commands=['testfetch'])
+def test_fetch_command(message):
     response_string = fetch_string()
     bot.reply_to(message, text=response_string, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['getPrefs'])
+@bot.message_handler(commands=['getprefs'])
 def get_prefs(message):
     prefs = f"<b>Mesghal</b>\n" \
-            f"{'uprate:' : <10}{convert_digit_en_fa(mesghal['uprate']) : >15}\n" \
-            f"{'downrate:' : <10} {convert_digit_en_fa(mesghal['downrate']) : >10}"
+            f"{'uprate(rial):' : <30}{convert_digit_en_fa(mesghal['uprate']) : ^20}\n" \
+            f"{'downrate(rial):' : <30}{convert_digit_en_fa(mesghal['downrate']) : ^20}\n" \
+            f"\n<b>Time</b>\n" \
+            f"{'periodic time(minute):' : <30}{convert_digit_en_fa(periodic_time) : ^20}"
 
+    print(prefs)
     bot.reply_to(message, prefs, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['setMeshgalUprate'])
+@bot.message_handler(commands=['setmeshgaluprate'])
 def set_mesghal_uprate_request(message):
-    msg = bot.reply_to(message, f"Please enter the integer value of the uprate for mesghal")
+    msg = bot.reply_to(message, f"Please enter the integer value of the up rate for mesghal")
     bot.register_next_step_handler(msg, set_mesghal_uprate)
 
 
-def set_mesghal_uprate(message):
-    uprate = message.text
-    if not uprate.isdigit():
-        msg = bot.reply_to(message, f"Mesghal uprate should a be number. Enter that again:")
-        bot.register_next_step_handler(msg, set_mesghal_uprate)
-        return
-
-    uprate = int(uprate)
-    if uprate < 0:
-        msg = bot.reply_to(message, f"Mesghal uprate should a be a positive number. Enter that again:")
-        bot.register_next_step_handler(msg, set_mesghal_uprate)
-        return
-
-    mesghal['uprate'] = uprate
-    bot.reply_to(message, f"Mesghal uprate successfully changed to : {mesghal['uprate']}")
+@bot.message_handler(commands=['setmeshgaldownrate'])
+def set_mesghal_downrate_request(message):
+    msg = bot.reply_to(message, f"Please enter the integer value of the down rate for mesghal")
+    bot.register_next_step_handler(msg, set_mesghal_downrate)
 
 
-bot.polling(none_stop=True)
+@bot.message_handler(commands=['setperiodictime'])
+def set_periodic_time_request(message):
+    msg = bot.reply_to(message, f"Please enter the new value for periodic time."
+                                f"\nIt must be a number between {consts.PERIODIC_TIME_MIN} to "
+                                f"{consts.PERIODIC_TIME_MAX}.\n"
+                                f"0 means it will turn off")
+    bot.register_next_step_handler(msg, set_periodic_time)
+    # start_polling_thread()
+
+
+# @periodic_request.job(interval=timedelta(minutes=periodic_time))
+# def periodic_request_interval():
+#     print('periodic_request_interval')
+#     fetch_command({})
+
+
+# def start_periodic_request():
+#     print('periodic call')
+#     periodic_request.start()
+
+
+# def start_polling_thread():
+#     print(f"start new thread with: {periodic_time}")
+#     periodic_thread = threading.Thread(target=start_periodic_request)
+#     periodic_thread.start()
+
+
+if __name__ == "__main__":
+    # try:
+    #     start_polling_thread()
+    # except KeyboardInterrupt:
+    #     periodic_request.stop()
+
+    print('bot polling')
+    bot.polling(none_stop=True)
